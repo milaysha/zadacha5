e75361d34f7bbf078bfa7a95ed028e43e0952a7a
milayshayarullina <milallina_0905@icloud.com>
2024-06-17 20:50:37 +0500

    Собрала цепочку до 8го элемента

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,26 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private final Deth8 deth8;
+    public Needle7(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth8.toString();
     }
 }
+/* public class Duck5 {
+    private Egg6 egg;
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
+    @Override
+    public String toString() {
+        return ", в утке яйцо " + egg.toString();
+    }
+} */
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-
+    private Duck5 duck;
+    public Rabbit4(Duck5 duck5) {
+        this.duck = duck5;
+    }
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
